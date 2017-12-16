858d26f8fb662e28f3c1d080800e902ebd52e9c5
marusechka <32398561+marusechka@users.noreply.github.com>
2017-12-16 19:12:13 +0300

    Update NUCovering.cpp

--- NUCovering.cpp
+++ NUCovering.cpp
@@ -24,6 +24,8 @@ int main()
 	double a = -10, b = 0, c = 24, d = 16;
 	
 	high_resolution_clock::time_point t1, t2;
+	__cilkrts_end_cilk();
+	__cilkrts_set_param("nworkers", "4");
 	
 	t1 = high_resolution_clock::now(); //засекли время
 	high_level_analysis main_object(a,b,c,d);
@@ -33,6 +35,7 @@ int main()
 	t2 = high_resolution_clock::now();
 	duration<double> duration = (t2 - t1);
 	std::cout << "Длительность выполнения программы: " << duration.count() << " секунд" << std::endl;
+	std::cout << "Число вычислителей: " << __cilkrts_get_nworkers() << std::endl;
 
 	// Внимание! здесь необходимо определить пути до выходных файлов!
 	//const char* out_files[3] = {	"C:/Users/Marusik/Desktop/Project/sol.txt", "C:/Users/Marusik/Desktop/Project/notsol.txt", "C:/Users/Marusik/Desktop/Project/bound.txt" };
